9f415c1613108d051a39681a1275bde65b080b12
EstellaKSL <estellaksl@gmail.com>
2021-01-12 18:32:33 +0800

    restructure

---
+++ src/components/core/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './layout.module.css'
+
+export default function Layout() {
+  return (
+    <div>
+      {/* Add your elements here, make sure to break it to clear functions*/}
+    </div>
+  )
+}
+
+Layout.defaultProps = {}
